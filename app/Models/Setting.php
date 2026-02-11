bf781b30e6303e2a7aa9c81a060bbd0da6d14218
muhammad mamdouh <muhammadsobhi.24@gmail.com>
2026-02-11 18:33:46 +0200

    Update Setting.php

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -8,4 +8,9 @@ class Setting extends Model
 {
     // This allows us to use Setting::updateOrCreate
     protected $fillable = ['key', 'value'];
+
+    // Automatically convert JSON string from DB to Array in React
+    protected $casts = [
+        'value' => 'json',
+    ];
 }
